487f33ae6c53f77aeea78936c6ce95c54710ad6e
Shivam Shandilya <70915754+legitShivam@users.noreply.github.com>
2023-03-13 10:02:09 +0530

    chore: remove test bytecode

--- test.java
+++ test.java
@@ -1,9 +1,30 @@
-import java.math.BigDecimal;
-import java.util.Scanner;
+
 
 public class test {
     public static void main(String[] args) {
-        String str = "u";
-        System.out.println(str.equalsIgnoreCase(null));
+        Person p1 = new Person("Shivam", 21);
+        Person p2 = new Person("Shivam", 21);
+        Person p3 = p1;
+
+        System.out.println(p1.hashCode());
+        System.out.println(p2.hashCode());
+        System.out.println(p3.hashCode());
+
+        System.out.println(p1.equals(p3));
+    }
+}
+
+class Person{
+    private String name;
+    private int age;
+    
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void hello(){
+        System.out.println("Hello");
     }
 }
